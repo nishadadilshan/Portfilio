156a41faef127330c7671c12c0a9f00939867d20
Dilshan Weerathunga <dilshan.weerathunga@synapsys.lk>
2025-07-31 00:42:45 +0200

    Changed copyright year

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export const Footer = () => {
   return (
     <footer className="footer">
-      Copyright &#169; 2024 Dilshan Weerathunga. All right received.
+      Copyright &#169; 2025 Dilshan Weerathunga. All right received.
     </footer>
   );
 };

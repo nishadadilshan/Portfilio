f48481203141352caf21e086cbe571c2411ead6d
Dilshan Weerathunga <dilshan.weerathunga@synapsys.lk>
2025-08-17 00:55:09 +0200

    added more technical cards

--- src/components/screen/Intro/TechnicalExpertise.js
+++ src/components/screen/Intro/TechnicalExpertise.js
@@ -7,9 +7,20 @@ const TechnicalExpertise = () => {
   const [counts, setCounts] = useState({ projects: 0, experience: 0, clients: 0 });
   const [skillPercentages, setSkillPercentages] = useState({
     react: 0,
+    reactNative: 0,
+    typescript: 0,
     javascript: 0,
     nodejs: 0,
-    css: 0
+    java: 0,
+    springBoot: 0,
+    html: 0,
+    css: 0,
+    git: 0,
+    firebase: 0,
+    cicd: 0,
+    agile: 0,
+    cloud: 0,
+    testing: 0
   });
 
   // Cursor following effect for stat items
@@ -58,7 +69,23 @@ const TechnicalExpertise = () => {
   }, []);
 
   useEffect(() => {
-    const targetSkills = { react: 90, javascript: 85, nodejs: 80, css: 95 };
+    const targetSkills = { 
+      react: 80, 
+      reactNative: 80, 
+      typescript: 85, 
+      javascript: 85, 
+      nodejs: 75, 
+      java: 60, 
+      springBoot: 60, 
+      html: 85, 
+      css: 80, 
+      git: 80, 
+      firebase: 75,
+      cicd: 60,
+      agile: 75,
+      cloud: 70,
+      testing: 75
+    };
     const duration = 1500;
     const steps = 50;
     const stepValue = duration / steps;
@@ -128,6 +155,30 @@ const TechnicalExpertise = () => {
             </div>
             <div className="skill-item">
               <div className="skill-info">
+                <span className="skill-name">React Native</span>
+                <span className="skill-percentage">{skillPercentages.reactNative}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.reactNative}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">TypeScript</span>
+                <span className="skill-percentage">{skillPercentages.typescript}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.typescript}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
                 <span className="skill-name">JavaScript</span>
                 <span className="skill-percentage">{skillPercentages.javascript}%</span>
               </div>
@@ -152,6 +203,42 @@ const TechnicalExpertise = () => {
             </div>
             <div className="skill-item">
               <div className="skill-info">
+                <span className="skill-name">Java</span>
+                <span className="skill-percentage">{skillPercentages.java}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.java}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">Spring Boot</span>
+                <span className="skill-percentage">{skillPercentages.springBoot}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.springBoot}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">HTML5</span>
+                <span className="skill-percentage">{skillPercentages.html}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.html}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
                 <span className="skill-name">CSS/SCSS</span>
                 <span className="skill-percentage">{skillPercentages.css}%</span>
               </div>
@@ -159,6 +246,78 @@ const TechnicalExpertise = () => {
                 <div 
                   className="skill-progress" 
                   style={{ width: `${skillPercentages.css}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">Git/GitHub</span>
+                <span className="skill-percentage">{skillPercentages.git}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.git}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">Firebase</span>
+                <span className="skill-percentage">{skillPercentages.firebase}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.firebase}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">CI/CD</span>
+                <span className="skill-percentage">{skillPercentages.cicd}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.cicd}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">Agile</span>
+                <span className="skill-percentage">{skillPercentages.agile}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.agile}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">Cloud</span>
+                <span className="skill-percentage">{skillPercentages.cloud}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.cloud}%` }}
+                ></div>
+              </div>
+            </div>
+            <div className="skill-item">
+              <div className="skill-info">
+                <span className="skill-name">Testing</span>
+                <span className="skill-percentage">{skillPercentages.testing}%</span>
+              </div>
+              <div className="skill-bar">
+                <div 
+                  className="skill-progress" 
+                  style={{ width: `${skillPercentages.testing}%` }}
                 ></div>
               </div>
             </div>
